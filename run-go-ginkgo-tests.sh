79de28d9ae09403fe8169862a15c69a6869e3934
freedomsean <t101598009@ntut.org.tw>
2020-06-09 14:14:48 +0800

    fix: wrong coveageDir

--- run-go-ginkgo-tests.sh
+++ run-go-ginkgo-tests.sh
@@ -6,6 +6,4 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-coverageDir = "$@" | "shippable/codecoverage"
-
-exec mkdir -p $coverageDir && ginkgo -r -cover -covermode=set -outputdir=$coverageDir/ -coverprofile=coverage.out && gocov convert $coverageDir/coverage.out | gocov-xml  > $coverageDir/coverage.xml
+exec coverageDir = "$@" | "shippable/codecoverage" && mkdir -p $coverageDir && ginkgo -r -cover -covermode=set -outputdir=$coverageDir/ -coverprofile=coverage.out && gocov convert $coverageDir/coverage.out | gocov-xml  > $coverageDir/coverage.xml
